f4c9d3eed44c0d51f8ced08b1ddba64db3ff57c1
Oleg <legich007@mail.ru>
2020-02-25 21:09:58 +0300

    add files

---
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Calculator;
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string oneOut = "1";
+        private const string twoOut = "2";
+        private const string threeOut = "3";
+        private const string fourOut = "4";
+        private const string fiveOut = "5";
+        private const string sixOut = "6";
+        private const string sevenOut = "7";
+        private const string eightOut = "8";
+        private const string nineOut = "9";
+        private const string zeroOut = "0";
+        private const int DEFAULT_WIDTH = 355;
+        private const int DEFAULT_HEIGHT = 350;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClassicTool.IsEnabled = false;
+            EngeneerTool.IsEnabled = true;
+            this.Width = DEFAULT_WIDTH;
+            this.Height = DEFAULT_HEIGHT;
+        }
+
+
+
+        private void KeyExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(zeroOut);
+        }
+
+        private void KeyOne_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(oneOut);
+        }
+
+        private void KeyTwo_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(twoOut);
+        }
+
+        private void KeyThree_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(threeOut);
+        }
+
+        private void KeyFour_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(fourOut);
+        }
+
+        private void KeyFive_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(fiveOut);
+        }
+
+        private void KeySix_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(sixOut);
+        }
+
+        private void KeySeven_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(sevenOut);
+        }
+
+        private void KeyEight_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(eightOut);
+        }
+
+        private void KeyNine_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcNumber(nineOut);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcDecimal();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcSign();
+        }
+
+        private void KeyDivide_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eDivide);
+        }
+
+        private void KeyMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eMultiply);
+        }
+
+        private void KeyMinus_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eSubtract);
+        }
+
+        private void KeyPlus_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eAdd);
+        }
+
+        private void KeyEqual_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.CalcEqual();
+            CalcEngine.CalcSpecialReset();
+
+        }
+
+        private void KeyClear_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcReset();
+            OutputDisplay.Text = "0";
+        }
+
+        private void KeyDate_Click(object sender, RoutedEventArgs e)
+        {
+            OutputDisplay.Text = CalcEngine.GetDate();
+            CalcEngine.CalcReset();
+        }
+
+        private void EngeneerTool_Click(object sender, RoutedEventArgs e)
+        {
+            this.Width = 500;
+            ClassicTool.IsEnabled = true;
+            EngeneerTool.IsEnabled = false;
+        }
+
+        private void ClassicTool_Click(object sender, RoutedEventArgs e)
+        {
+            ClassicTool.IsEnabled = false;
+            EngeneerTool.IsEnabled = true;
+            this.Width = DEFAULT_WIDTH;
+            this.Height = DEFAULT_HEIGHT;
+        }
+
+        private void KeyDegree_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eDegree);
+        }
+
+        private void KeySQRT_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eSQRT);
+            OutputDisplay.Text = CalcEngine.CalcEqual();
+           // CalcEngine.CalcReset();
+        }
+
+        private void KeyReverse_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eDivByOne);
+            OutputDisplay.Text = CalcEngine.CalcEqual();
+           // CalcEngine.CalcReset();
+        }
+
+        private void KeyDegree2_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eDegree2);
+            OutputDisplay.Text = CalcEngine.CalcEqual();
+           // CalcEngine.CalcReset();
+        }
+
+        private void KeyFactorial_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eFactorial);
+            OutputDisplay.Text = CalcEngine.CalcEqual();
+           // CalcEngine.CalcReset();
+        }
+
+        private void KeyCBRT_Click(object sender, RoutedEventArgs e)
+        {
+            CalcEngine.CalcOperation(CalcEngine.Operator.eCBRT);
+            OutputDisplay.Text = CalcEngine.CalcEqual();
+           // CalcEngine.CalcReset();
+        }
+
+        private void KeyQE_Click(object sender, RoutedEventArgs e)
+        {
+            QEWindow formQE = new QEWindow();
+            formQE.Owner = this;
+            formQE.Show();
+        }
+    }
+}
---
+++ CalculatorWPF/QEWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using SimpleCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace CalculatorWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для QEWindow.xaml
+    /// </summary>
+    public partial class QEWindow : Window
+    {
+        public QEWindow()
+        {
+            InitializeComponent();
+        }
+        protected string a
+        {
+            get { return textBoxA.Text; }
+            set { textBoxA.Text = value; }
+        }
+
+        protected string b
+        {
+            get { return textBoxB.Text; }
+            set { textBoxB.Text = value; }
+        }
+
+        protected string c
+        {
+            get { return textBoxC.Text; }
+            set { textBoxC.Text = value; }
+        }
+
+        
+
+       
+
+        private void CalcButton_Click(object sender, RoutedEventArgs e)
+        {
+            double x1;
+            double x2;
+
+            try
+            {
+                double a1 = double.Parse(a);
+                double b1 = double.Parse(b);
+                double c1 = double.Parse(c);
+                if (Ur.Deskr(a1, b1, c1) >= 0)
+                {
+                    Ur.Gg(a1, b1, c1, out x1, out x2);
+                    richTextBox1.Text = $"{a}*x^2 + {b}*x + {c} = 0\n" + $"Корни уравнения: \nx1 = {x1}\nx2 = {x2} ";
+                }
+                else richTextBox1.Text = "Корней нет";
+                //richTextBox1.
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные");
+
+            }
+
+
+        }
+
+        private void textBoxA_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            double val;
+            if (!Double.TryParse(e.Text, out val) && e.Text != "-" && e.Text!=".")
+            {
+                e.Handled = true; // отклоняем ввод
+            }
+        }
+
+        private void textBoxB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            double val;
+            if (!Double.TryParse(e.Text, out val) && e.Text != "-" && e.Text != ".")
+            {
+                e.Handled = true; // отклоняем ввод
+            }
+        }
+
+        private void textBoxC_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            double val;
+            if (!Double.TryParse(e.Text, out val) && e.Text != "-" && e.Text != ".")
+            {
+                e.Handled = true; // отклоняем ввод
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxA.Clear();
+            textBoxB.Clear();
+            textBoxC.Clear();
+            richTextBox1.Clear();
+        }
+    }
+}
